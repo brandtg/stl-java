090a3bfd916c8299deaa9b6d04acf2af06a3a5fc
Greg Brandt <brandt.greg@gmail.com>
2015-10-22 12:55:16 -0700

    Moved some stuff to test

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ A Java implementation of [STL: A Seasonal-Trend Decomposition Procedure Based on
 Example
 -------
 
-The following shows the result of running `STLDecomposition` on `src/test/resources/sample-timeseries.json`, plotted using `src/main/resources/plot-series.py`
+The following shows the result of running `STLRunner` on `src/test/resources/sample-timeseries.json`, plotted using `src/test/resources/plot-series.py`
 
 ![STL result chart](doc/figure_1.png)
--- src/main/java/com/github/brandtg/STLDecomposition.java
+++ src/main/java/com/github/brandtg/STLDecomposition.java
@@ -1,7 +1,5 @@
 package com.github.brandtg;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.math3.analysis.interpolation.LoessInterpolator;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
@@ -300,64 +298,5 @@ public class STLDecomposition {
     }
 
     return movingAverage;
-  }
-
-  /**
-   * Accepts an input JSON file containing raw time series, e.g.
-   *
-   * <pre>
-   *   {
-   *     "times": [1,2,3,4,...],
-   *     "series": [100,200,300,400,...]
-   *   }
-   * </pre>
-   *
-   * And outputs the STL de-trended data:
-   *
-   * <pre>
-   *   {
-   *     "times": [1,2,3,4,...],
-   *     "series": [100,200,300,400,...],
-   *     "seasonal": [...],
-   *     "trend": [...],
-   *     "remainder": [...]
-   *   }
-   * </pre>
-   *
-   * n.b. This isn't really meant to be used on anything besides src/test/resources/sample-timeseries.json,
-   * just an example.
-   */
-  public static void main(String[] args) throws Exception {
-    if (args.length != 2) {
-      System.err.println("usage: input_file output_file");
-      System.exit(1);
-    }
-
-    ObjectMapper objectMapper = new ObjectMapper();
-    JsonNode tree = objectMapper.readTree(new File(args[0]));
-    int n = tree.get("times").size();
-    long[] tsLong = new long[n];
-    double[] ts = new double[n];
-    double[] ys = new double[n];
-
-    for (int i = 0; i < n; i++) {
-      tsLong[i] =  tree.get("times").get(i).asLong();
-      ts[i] = tree.get("times").get(i).asDouble();
-      ys[i] = tree.get("series").get(i).asDouble();
-    }
-
-    // This configuration was chosen to work with monthly data over 20 years
-    STLConfig config = new STLConfig();
-    config.setNumberOfObservations(12);
-    config.setNumberOfInnerLoopPasses(10);
-    config.setNumberOfRobustnessIterations(1);
-    config.setSeasonalComponentBandwidth(0.75);
-    config.setLowPassFilterBandwidth(0.30);
-    config.setTrendComponentBandwidth(0.10);
-    config.setNumberOfDataPoints(ts.length);
-    STLDecomposition stl = new STLDecomposition(config);
-    STLResult res = stl.decompose(tsLong, ys);
-
-    objectMapper.writerWithDefaultPrettyPrinter().writeValue(new FileOutputStream(args[1]), res);
   }
 }
